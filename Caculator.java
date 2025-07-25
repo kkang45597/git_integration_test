63a1081ffff7b78503770f77e3d0424e2d8bf5e6
kkang4557 <kkang45597@gmail.com>
2025-07-25 10:30:00 +0900

    pc2 edit

--- Caculator.java
+++
@@ -1,9 +0,0 @@
-public class Caculator {
-    public static void main(String[] args) {
-        System.out.println("1+1=2");
-        System.out.println("1-1=0");
-        System.out.println("2*4=8");
-        System.out.println("4/2=2");
-        // 학원에서 수정
-    }
-}
