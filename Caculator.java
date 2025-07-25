188e419c2dd415ab59bb3f17ea3e8e77ff6421c6
kkang4557 <kkang45597@gmail.com>
2025-07-25 10:30:54 +0900

    edit home

---
+++ Caculator.java
@@ -0,0 +1,10 @@
+public class Caculator {
+    public static void main(String[] args) {
+        System.out.println("1+1=2");
+        System.out.println("1-1=0");
+        System.out.println("2*4=8");
+        System.out.println("4/2=2");
+        // 학원에서 수정
+        // 집에서 수정
+    }
+}
