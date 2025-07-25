d6f8b6d0c89e19bb6a41a07b20b91f2e378a6a9f
kkang4557 <kkang45597@gmail.com>
2025-07-25 10:15:08 +0900

    곱셉 추가

--- Caculator.java
+++ Caculator.java
@@ -1,6 +1,7 @@
 public class Caculator {
     public static void main(String[] args) {
         System.out.println("1+1=2");
-        System.out.println("1-1=2");
+        System.out.println("1-1=0");
+        System.out.println("2+6=9");
     }
 }
